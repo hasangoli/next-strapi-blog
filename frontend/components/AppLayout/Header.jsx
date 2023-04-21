3155b9559dd47f4a9b02abf533f5bf8ae821830c
Sann <mrhasangoli@gmail.com>
2023-04-21 15:33:40 +0430

    initial

--- frontend/components/AppLayout/Header.jsx
+++ frontend/components/AppLayout/Header.jsx
@@ -1,43 +1,148 @@
+import MenuIcon from '@mui/icons-material/Menu';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Toolbar from '@mui/material/Toolbar';
+import Tooltip from '@mui/material/Tooltip';
+import Typography from '@mui/material/Typography';
 import Link from 'next/link';
+import { useState } from 'react';
 import ImageProvider from '../Global/ImageProvider';
 
 const Header = () => {
+  const menuItems = [
+    { title: 'Home', href: '/' },
+    { title: 'Blogs', href: '/blogs' },
+    { title: 'Categories', href: '/categories' },
+    { title: 'Authors', href: '/authors' },
+    { title: 'Contact Us', href: '/contact' },
+    { title: 'About Us', href: '/about' },
+  ];
+
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const handleOpenNavMenu = event => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = event => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <header className='bg-black text-primary'>
-      <div className='container py-2 px-3 sm:px-0'>
-        <div className='flex flex-row items-center justify-between w-full'>
-          <div>
-            <h1>BLOG</h1>
-          </div>
-          <nav className='fixed left-0 top-0 bg-dark w-full h-full z-10'>
-            <ul className='flex flex-col'>
-              <li className='mx-2'>
-                <Link href='/'>Home</Link>
-              </li>
-              <li className='mx-2'>
-                <Link href='/blogs'>Blogs</Link>
-              </li>
-              <li className='mx-2'>
-                <Link href='/categories'>Categories</Link>
-              </li>
-              <li className='mx-2'>
-                <Link href='/about'>About Us</Link>
-              </li>
-            </ul>
-          </nav>
-          <div>
-            <ImageProvider
-              src='/assets/images/blog/09.jpg'
-              alt=''
-              width={'2.5rem'}
-              aspectRatio={1}
-              className={'rounded-full overflow-hidden'}
-            />
-          </div>
-        </div>
+    <AppBar position='sticky' className='bg-darker'>
+      <div className='container'>
+        <Toolbar>
+          <h6 className='hidden md:flex me-8 font-bold tracking-widest uppercase text-2xl'>
+            Blog
+          </h6>
+
+          <Box className='flex md:hidden'>
+            <IconButton
+              size='large'
+              aria-label='account of current user'
+              aria-controls='menu-appbar'
+              aria-haspopup='true'
+              onClick={handleOpenNavMenu}
+              color='inherit'
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id='menu-appbar'
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              className='md:none'
+            >
+              {menuItems.map((item, i) => (
+                <MenuItem key={i}>
+                  <Link href={item?.href}>
+                    <Typography textAlign='center'>{item?.title}</Typography>
+                  </Link>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant='h5'
+            noWrap
+            component='a'
+            href=''
+            className='flex md:hidden font-bold mx-auto tracking-widest'
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {menuItems.map((item, i) => (
+              <Button
+                key={i}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {item?.title}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip>
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <ImageProvider
+                  src='/assets/images/blog/09.jpg'
+                  alt=''
+                  width={'2.5rem'}
+                  aspectRatio={1}
+                  className={'rounded-full overflow-hidden'}
+                />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id='menu-appbar'
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {['Profile', 'Add Post', 'Logout'].map(setting => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign='center'>{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
       </div>
-    </header>
+    </AppBar>
   );
 };
-
 export default Header;
