97f01949f5c63065c3b1c8a070b6229301921b95
Hasan <mrhasangoli@gmail.com>
2023-06-02 13:03:41 +0330

    initial

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['localhost', '127.0.0.1'],
-    deviceSizes: [280, 320, 480, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-  },
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,24 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './src/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
-    colors: {
-      primary: '#FFD369',
-      darker: '#222831',
-      dark: '#393E46',
-      light: '#EEEEEE',
-      white: '#FFF',
-      black: '#000',
-    },
-    container: {
-      center: true,
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
     },
   },
   plugins: [],
-};
+}
